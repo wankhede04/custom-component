bde1dfaa00ad20d6ac8621a2256c7f69c1023dd8
wankhede04 <wankhedevijay04@gmail.com>
2020-05-23 21:46:44 +0530

    Added dashboard component.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, AboutComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [HeaderComponent, FooterComponent]
 })
 export class SharedModule { }
