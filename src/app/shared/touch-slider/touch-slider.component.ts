82dba2ccdd893fd89c194e19af1282c0672b6b81
wankhede04 <wankhedevijay04@gmail.com>
2020-05-24 20:43:12 +0530

    Added tocuh slider at dashboard.

--- src/app/shared/touch-slider/touch-slider.component.ts
+++ src/app/shared/touch-slider/touch-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-touch-slider',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./touch-slider.component.scss']
 })
 export class TouchSliderComponent implements OnInit {
+
+  @Input()
+  public boxDetails: Array<any>;
 
   constructor() { }
 
