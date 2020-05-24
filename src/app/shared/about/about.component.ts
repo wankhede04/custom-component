3a2553ea07dab4e211db5431ca5dd35a21e37b85
wankhede04 <wankhedevijay04@gmail.com>
2020-05-24 15:36:21 +0530

    Added about.

--- src/app/shared/about/about.component.ts
+++ src/app/shared/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activeModal: NgbActiveModal
+  ) { }
 
   ngOnInit() {
+  }
+
+  public dismissModal() {
+    this.activeModal.dismiss();
   }
 
 }
