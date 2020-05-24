5f83e34915c21390eec445a6b5e8ba3c849374b5
wankhede04 <wankhedevijay04@gmail.com>
2020-05-24 19:54:32 +0530

    Updated ap routing module to redirect to dashboard on first load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent}
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  { path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
