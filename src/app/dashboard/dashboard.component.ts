5606c3f4521cf9bc8ff86203c082de9f023db7fa
wankhede04 <wankhedevijay04@gmail.com>
2020-05-24 20:43:43 +0530

    Added character details for touch slider.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ICharacterDetails } from '../models/CardDetails';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  public characterDetails: Array<ICharacterDetails> = [
+    {
+      title: 'skyler',
+      quote: 'I need you to look me in the eye and tell me right now. Are you in danger?',
+      fullName: 'Skyler White'
+    },
+    {
+      title: 'walter-jr',
+      quote: 'If you’re going to buy me off, buy me off.',
+      fullName: 'Walter White, Jr.'
+    },
+    {
+      title: 'lydia',
+      quote: '',
+      fullName: 'Lydia Rodarte'
+    },
+    {
+      title: 'walter',
+      quote: 'What? does he have some kind of sixth sense.',
+      fullName: 'Walter White'
+    },
+    {
+      title: 'jesse',
+      quote: 'Did you just bring a bomb into a hospital?',
+      fullName: 'Jesse Pinkman'
+    },
+    {
+      title: 'saul',
+      quote: 'Celebrities get their cars washed just like everybody else.',
+      fullName: 'Saul Goodman'
+    },
+    {
+      title: 'hank',
+      quote: 'Are you seriously doing this to me again?',
+      fullName: 'Hank Shrader'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
