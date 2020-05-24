456018075cd050749b50624ce3354f995893c90b
wankhede04 <wankhedevijay04@gmail.com>
2020-05-24 18:12:24 +0530

    Added touch slider

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TouchSliderComponent } from './touch-slider/touch-slider.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, AboutComponent],
+  declarations: [HeaderComponent, FooterComponent, AboutComponent, TouchSliderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ],
-  exports: [HeaderComponent, FooterComponent]
+  exports: [HeaderComponent, FooterComponent, TouchSliderComponent],
+  entryComponents: [AboutComponent]
 })
 export class SharedModule { }
