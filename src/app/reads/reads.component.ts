46c37f0bf795bce82fc958cd0acdb70b2c2aab06
wankhede04 <wankhedevijay04@gmail.com>
2020-05-26 22:51:00 +0530

    Added reads component

---
+++ src/app/reads/reads.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reads',
+  templateUrl: './reads.component.html',
+  styleUrls: ['./reads.component.scss']
+})
+export class ReadsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
