a08b85c44baae0c84255403d77146f6671c301e2
wankhede04 <wankhedevijay04@gmail.com>
2020-05-26 22:50:24 +0530

    Fixed the issue with Swiper not updating when chaning the route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+import { ReadsComponent } from './reads/reads.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
+  { path: 'home', component: HomeComponent},
   { path: 'dashboard', component: DashboardComponent},
-  { path: 'details', component: DetailsComponent}
+  { path: 'details', component: DetailsComponent},
+  { path: 'reads', component: ReadsComponent}
 ];
 
 @NgModule({
--- src/app/shared/touch-slider/touch-slider.component.ts
+++ src/app/shared/touch-slider/touch-slider.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
+declare var Swiper: any;
+
 @Component({
   selector: 'app-touch-slider',
   templateUrl: './touch-slider.component.html',
@@ -10,12 +12,33 @@ export class TouchSliderComponent implements OnInit {
 
   @Input()
   public boxDetails: Array<any>;
+  swiper: any;
 
   constructor(
     private router: Router
   ) { }
 
   ngOnInit() {
+    this.swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+  }
+
+  ngAfterViewInit() {
+    this.swiper.update();
   }
 
   public goToDetails() {
