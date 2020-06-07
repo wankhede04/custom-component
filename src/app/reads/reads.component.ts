a2112925eed7c484b043f7806d71da1deb770e08
wankhede04 <wankhedevijay04@gmail.com>
2020-06-07 17:15:03 +0530

    Added the card-fx at read component

--- src/app/reads/reads.component.ts
+++ src/app/reads/reads.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reads.component.scss']
 })
 export class ReadsComponent implements OnInit {
+  public counts: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8];
 
   constructor() { }
 
