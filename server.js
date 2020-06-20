c669abfa713c720f3a67d56cb2de283ca42bce90
wankhede04 <wankhedevijay04@gmail.com>
2020-06-20 22:53:42 +0530

    Updated the path

--- server.js
+++ server.js
@@ -33,5 +33,5 @@ app.listen(process.env.PORT || 8080);
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/custom-component/index.html'));
 });
